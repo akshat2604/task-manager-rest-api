366a880a03179a2270533ccc6ca6d9e7802f1440
Akshat Jain <akshatjain2604@gmail.com>
2020-05-09 17:53:41 +0530

    add admin male as env variable

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,17 +2,10 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.sendgridAPIkey);
 
-sgMail.send({
-    to: 'bhutaakshat@gmail.com',
-    from: 'akshatjain2604@gmail.com',
-    subject: 'This is my first ceation.',
-    text: 'I hope it works fine.'
-});
-
 const sendwelcomeemail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'akshatjain2604@gmail.com',
+        from: process.env.devemail,
         subject: 'Welcome to Task-App!',
         text: `Hey ${name} You are now part of Task-app family.\nNow manage all your tasks in one app.`
     });
@@ -21,7 +14,7 @@ const sendwelcomeemail = (email, name) => {
 const sendremoveemail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'akshatjain2604@gmail.com',
+        from: process.env.devemail,
         subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}.\nWe hope to see you back somtime soon.`
     });
